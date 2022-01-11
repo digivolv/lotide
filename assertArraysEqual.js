0f804cbe22fac839ed46b182d9c562c518b8a07e
Edwin Ly <edwinjly@gmail.com>
2022-01-11 16:47:34 -0500

    implemented asserArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -22,6 +22,5 @@ const eqArrays = (arrayOne, arrayTwo) => {
 // TEST CODE
 // eqArrays([1, 2, 3], [1, 2, 3]); // => true
 // eqArrays([1, 2, 3], [3, 2, 1]); // => false
-
-eqArrays(["1", "2", "3"], ["1", "1", "3"]); // => true?
+//eqArrays(["1", "2", "3"], ["1", "1", "3"]); // => true?
 //eqArrays(["1", "2", "3"], ["1", "2", 3]);
