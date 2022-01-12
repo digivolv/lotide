2ad4a495e9fbb736f9f4ddcc724ec9b9b7d02de3
Edwin Ly <edwinjly@gmail.com>
2022-01-11 20:36:11 -0500

    implemented middle.js

--- middle.js
+++ middle.js
@@ -1,16 +1,20 @@
 const middle = (array) => {
   let middleIndex = [];
-
-  if (array.length % 2 === 0) {
+  if (array.length <= 2) {
+    return middleIndex;
+  } else if (array.length % 2 === 0) {
     middleIndex.push(Math.ceil(array.length / 2));
     middleIndex.push(Math.ceil(array.length / 2) + 1);
   } else if (!(array.length % 2 === 0)) {
     middleIndex.push(Math.ceil(array.length / 2));
   }
-  console.log(middleIndex);
   return middleIndex;
 };
 
-middle([1, 2, 4]);
+console.log(middle([1, 2, 4]));
 middle([1, 2, 3, 4]); // => [2, 3]
-middle([1, 2, 3, 4, 5, 6]); // => [3, 4]
+console.log(middle([1, 2, 3, 4, 5, 6])); // => [3, 4]
+middle([1, 2, 3]); // => [2]
+console.log(middle([1, 2, 3, 4, 5])); // => [3]
+console.log(middle([1])); // => []
+console.log(middle([1, 2])); // => []
