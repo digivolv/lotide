4467011471795b2b100d2660c84d85f39d19c322
Edwin Ly <edwinjly@gmail.com>
2022-01-12 16:41:11 -0500

    implemented letterPositions.js

---
+++ letterPositions.js
@@ -0,0 +1,46 @@
+const assertArraysEqual = (arrayOne, arrayTwo) => {
+  let doesThisPass = true;
+  // ===
+  //== for null / undefined false values
+  //check array lengths are equal
+
+  for (let i = 0; i < arrayOne.length; i++) {
+    if (arrayOne[i] === arrayTwo[i]) {
+      console.log(
+        `✅ Assertion Passed at index ${i}: ${arrayOne[i]} === ${arrayTwo[i]}`
+      );
+    } else if (arrayOne[i] !== arrayTwo[i]) {
+      console.log(
+        `🛑 Assertion Failed at index ${i}: ${arrayOne[i]} !== ${arrayTwo[i]}`
+      );
+      doesThisPass = false;
+    }
+  }
+
+  return doesThisPass
+    ? console.log("✅✅✅ The two arrays are equivalent: " + doesThisPass)
+    : console.log("🛑🛑🛑 The two arrays are not equivalent: " + doesThisPass);
+};
+
+const letterPositions = (sentence) => {
+  const results = {};
+  // stripped = sentence.replace(/\s/g, "");
+  for (let i = 0; i < sentence.length; i++) {
+    if (!results[sentence[i]] && sentence[i] !== " ") {
+      results[sentence[i]] = [];
+      results[sentence[i]].push(i);
+      console.log("results[sentence[i]]: " + results[sentence[i]]);
+    } else if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    }
+  }
+  return results;
+};
+
+console.log(assertArraysEqual(letterPositions("hello").e, [1]));
+console.log(
+  assertArraysEqual(
+    letterPositions("lighthouse in the house").h,
+    [3, 5, 15, 18]
+  )
+);
