fd5756d0829d02ad366f24609bc87a920ea2ce98
Edwin Ly <edwinjly@gmail.com>
2022-01-13 17:05:27 -0500

    findKey.js

---
+++ findKey.js
@@ -0,0 +1,52 @@
+// // Implement the function findKey which takes
+//  in an object and a callback. It should scan the object
+//  and return the first key for which the callback returns a
+//   truthy value. If no key is found, then
+//    it should return undefined.
+
+const findKey = (object, callback) => {
+  let keys = Object.keys(object);
+  for (const key of keys) {
+    if (callback(object[key])) {
+      // console.log(key);
+      return key;
+    }
+  }
+};
+
+findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 2
+); // => "noma"
+
+const assertEqual = function (actual, expected) {
+  console.log(actual === expected);
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+  return actual === expected;
+};
+
+assertEqual(
+  findKey(
+    {
+      "Blue Hill": { stars: 1 },
+      Akaleri: { stars: 3 },
+      noma: { stars: 2 },
+      elBulli: { stars: 3 },
+      Ora: { stars: 2 },
+      Akelarre: { stars: 3 },
+    },
+    (x) => x.stars === 2
+  ),
+  "noma"
+);
