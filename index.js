6d953d8ba1322d1a01aca4be69c9e185f9b9df10
Edwin Ly <edwinjly@gmail.com>
2022-01-17 16:45:31 -0500

    index.js implemented object shorthand

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const tail = require("./tail");
 const middle = require("./middle");
 
 module.exports = {
-  head: head,
-  tail: tail,
-  middle: middle,
-  eqArrays: eqArrays,
-  assertEqual: assertEqual,
-  assertArraysEqual: assertArraysEqual,
+  head,
+  tail,
+  middle,
+  eqArrays,
+  assertEqual,
+  assertArraysEqual,
 };
